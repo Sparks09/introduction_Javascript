65bd7f3179d2b252e99e497ce7a8b7002ab76b5d
Sparks09 <101443282+Sparks09@users.noreply.github.com>
2022-06-24 05:17:40 +0100

    A javascript code that prints out your name, height, and country on the screen

---
+++ introduction.js
@@ -0,0 +1,6 @@
+// Write a javascript code that prints out your name, height, and country on the screen
+var n, h, c 
+n = prompt("Enter your name")
+h = prompt("Enter your height")
+c = prompt("Enter your country name")
+document.write(n + "<br>" + h + "<br>" + c )
